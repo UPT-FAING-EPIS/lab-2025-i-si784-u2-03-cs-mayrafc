d18fdbe10c7d89d1e4d51149cba096cb4dfa38a3
mayrafc <mc2021072620@virtual.upt.pe>
2025-05-24 19:09:59 -0500

    actividad 1

--- Bank/Bank.Domain.Tests/BanckAccountTests.cs
+++ Bank/Bank.Domain.Tests/BanckAccountTests.cs
@@ -47,8 +47,39 @@ namespace Bank.Domain.Tests
             catch (System.ArgumentOutOfRangeException e)
             {
                 // Assert
-            Assert.That(e.Message, Does.Contain(BankAccount.DebitAmountExceedsBalanceMessage));
+                Assert.That(e.Message, Does.Contain(BankAccount.DebitAmountExceedsBalanceMessage));
             }
         }
+        
+        [Test]
+        public void Credit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double creditAmount = 5.77;
+            double expected = beginningBalance + creditAmount;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Credit(creditAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.That(actual, Is.EqualTo(expected).Within(0.001), "Account not credited correctly");
+        }
+
+        [Test]
+        public void Credit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double creditAmount = -5.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act & Assert
+            var ex = Assert.Throws<System.ArgumentOutOfRangeException>(() => account.Credit(creditAmount));
+            Assert.That(ex.ParamName, Is.EqualTo("amount"));
+        }
+
     }
 }
