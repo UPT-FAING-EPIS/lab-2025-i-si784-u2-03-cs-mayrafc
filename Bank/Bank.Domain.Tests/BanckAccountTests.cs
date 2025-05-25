6da268166a92e5f7d4c66662d521090cc4e6ce27
mayrafc <mc2021072620@virtual.upt.pe>
2025-05-24 19:06:21 -0500

    lab2

---
+++ Bank/Bank.Domain.Tests/BanckAccountTests.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using Bank.Domain;
+
+
+namespace Bank.Domain.Tests
+{
+    public class BankAccountTests
+    {
+        [Test]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // Act
+            account.Debit(debitAmount);
+            // Assert
+            double actual = account.Balance;
+            Assert.That(actual, Is.EqualTo(expected).Within(0.001));
+        }
+
+        [Test]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // Act and assert
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+
+        [Test]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // Act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+            Assert.That(e.Message, Does.Contain(BankAccount.DebitAmountExceedsBalanceMessage));
+            }
+        }
+    }
+}
